f7a21ba81277c0ca9528116235f9b9e8897497c3
Chad Patterson <chapatte@cisco.com>
2017-12-21 14:54:26 -0500

    Initial commit

---
+++ getInvitePendingUsers.py
@@ -0,0 +1,59 @@
+import requests
+import csv
+import os
+import time
+
+
+def get_all(request, headers):
+    users = []
+    users.extend(request.json()['items'])
+    print(users)
+    while request.links:
+        request = requests.get(request.links['next']['url'], headers=headers)
+        users.extend(request.json()['items'])
+        time.sleep(0.1)
+    return users
+
+
+def get_users(auth):
+    url = 'https://api.ciscospark.com/v1/people'
+    headers = {'Authorization': 'Bearer {}'.format(auth)}
+    r = requests.get(url, headers=headers)
+    users = get_all(r, headers)
+    return users
+
+
+def filter_users(users):
+    filtered = []
+    for user in users:
+        if user['invitePending']:
+            filtered.append(user)
+    return filtered
+
+
+def write_to_csv(users):
+    headers = ['firstName', 'lastName', 'displayName', 'status', 'email', 'invitePending']
+    with open(os.path.expanduser("~/Desktop/Invite_Pending_users.csv"), 'w') as out:
+        writer = csv.DictWriter(out, headers, extrasaction='ignore')
+        writer.writeheader()
+        for user in users:
+            writer.writerow({'firstName': user.get('firstName', ''),
+                             'lastName': user.get('lastName', ''),
+                             'displayName': user.get('displayName', ''),
+                             'status': user.get('status', ''),
+                             'email': user['emails'][0],
+                             'invitePending': user['invitePending']})
+    return
+
+
+if __name__ == "__main__":
+    auth = input("Please Enter Spark Admin User Auth Token: ")
+    print('Gathering information...')
+    all_users = get_users(auth)
+    filtered_users = filter_users(all_users)
+    try:
+        write_to_csv(filtered_users)
+        print("Successfully created CSV file at ~/Desktop/Invite_Pending_Users.csv")
+    except Exception as e:
+        print('Error occurred while writing to file')
+        print('Error: {}'.format(e))
